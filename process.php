e7fd22e4fee03cec99ad980e92dadca3db3d5af8
backward99 <86753969+backward99@users.noreply.github.com>
2021-09-02 15:55:17 +0900

    Delete process.php

--- process.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "111111", "opentutorials1");
-$sql = "INSERT INTO topic1 (title,created,complted) VALUES('".$_POST['title']."', now(), '0000-00-00')";
-$result = mysqli_query($conn, $sql);
-header('Location: http://localhost/upload/index.php');
-?>
